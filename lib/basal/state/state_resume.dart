94ca58dfd680fa7a0fd417d2b764bd9db839b074
wangrundong <wangrundong@roobo.com>
2022-04-29 10:25:58 +0800

    【王润东】修复Resumeable，NullSafe支持时数据类型异常转换的问题

--- lib/basal/state/state_resume.dart
+++ lib/basal/state/state_resume.dart
@@ -6,82 +6,53 @@ class Resume {
 }
 
 abstract class ResumableState<T extends StatefulWidget> extends State<T> with WidgetsBindingObserver {
-  Resume resumeData = new Resume();
+  Resume resume = new Resume();
   bool _isPaused = false;
 
   //处理LessonLifecycle 中改为resume状态时 子widget（如：倒计时widget）连续多次调用setState()方法 导致多次build() 多次调用resume()
   bool resumeHasCall = false;
 
+  /// Implement your code here
   void onResume() {
   }
 
+  /// Implement your code here
   void onReady() {
   }
 
+  /// Implement your code here
   void onPause() {
   }
 
-  /// 代替Navigator.push(),
-  Future<T> push<T extends Object>(BuildContext context, Route<T> route, [String? source]) {
+  /// This method is replacement of Navigator.push(), but fires onResume() after route popped
+  Future<T?> push<T extends Object?>(BuildContext context, Route<T> route, [String? source]) {
     _isPaused = true;
     onPause();
 
-    return Navigator.of(context).push(route).then((value) {
+    return Navigator.of(context).push<T>(route).then((value) {
       _isPaused = false;
 
-      resumeData.data = value;
-      resumeData.source = source;
+      resume.data = value;
+      resume.source = source;
 
-      onResume();
-      return value!;
-    });
-  }
-
-  /// 代替showDialog,
-  Future<T> showLifecycleDialog<T>({
-    required BuildContext context,
-    WidgetBuilder? builder,
-    bool barrierDismissible = true,
-    Color? barrierColor,
-    bool useSafeArea = true,
-    bool useRootNavigator = true,
-    RouteSettings? routeSettings,
-    @Deprecated('child-is useless please use builder') Widget? child,
-  }) {
-    _isPaused = true;
-    onPause();
-
-    return showDialog(
-      context: context,
-      barrierDismissible: barrierDismissible,
-      barrierColor: barrierColor,
-      useSafeArea: useSafeArea,
-      useRootNavigator: useRootNavigator,
-      routeSettings: routeSettings,
-      builder: (BuildContext context) {
-        return child!;
-      },
-    ).then((value) {
-      _isPaused = false;
-      resumeData.data = value;
       onResume();
       return value;
     });
   }
 
-  /// 代替Navigator.pushNamed()
-  Future<T> pushNamed<T extends Object>(BuildContext context, String routeName, {Object? arguments}) {
+  /// This method is replacement of Navigator.pushNamed(), but fires onResume() after route popped
+  Future<T?> pushNamed<T extends Object?>(BuildContext context, String routeName, {Object? arguments}) {
     _isPaused = true;
     onPause();
 
     return Navigator.of(context).pushNamed<T>(routeName, arguments: arguments).then((value) {
       _isPaused = false;
 
-      resumeData.data = value;
-      resumeData.source = routeName;
+      resume.data = value;
+      resume.source = routeName;
 
       onResume();
-      return value!;
+      return value;
     });
   }
 
